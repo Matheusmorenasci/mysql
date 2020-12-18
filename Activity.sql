9cec5fe830f7b1bcb2fef2de38175aed23a8f2cd
Matheus Moreno Nascimento <matheusmorenasci@gmail.com>
2020-12-18 16:31:26 -0300

    Update Activity.sql

--- Activity.sql
+++ Activity.sql
@@ -1,38 +1,38 @@
-CREATE DATABASE carros;
-USE carros;
+CREATE DATABASE cars;
+USE cars;
 
-CREATE TABLE marca (
-	codigo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(200) NOT NULL,
-	pais VARCHAR(20) NOT NULL
+CREATE TABLE brand (
+	code INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(200) NOT NULL,
+	country VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE modelo (
-	codigo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(200) NOT NULL,
-	categoria VARCHAR(100) NOT NULL,
-	cor VARCHAR(100) NOT NULL,
-	codigo_marca INT NOT NULL
+CREATE TABLE model (
+	code INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(200) NOT NULL,
+	category VARCHAR(100) NOT NULL,
+	color VARCHAR(100) NOT NULL,
+	brand_code INT NOT NULL
 );
 
-ALTER TABLE modelo
-	ADD CONSTRAINT FK_marca
-	FOREIGN KEY (codigo_marca)
-	REFERENCES marca (codigo);
+ALTER TABLE model
+	ADD CONSTRAINT FK_brand
+	FOREIGN KEY (brand_code)
+	REFERENCES brand (code);
 	
-INSERT INTO marca (nome, pais) VALUES ('Volkswagen', 'Alemanha'), ('Ford', 
-'EUA'), ('Fiat', 'Itália'), ('GM', 'EUA');
+INSERT INTO brand (name, country) VALUES ('Volkswagen', 'Germany'), ('Ford', 
+'USA'), ('Fiat', 'Italy'), ('GM', 'USA');
 
-INSERT INTO modelo (nome, categoria, cor, codigo_marca) VALUES ('Gol', 'Hatch', 
-'preto', 1), ('Palio', 'Hatch', 'vermelho', 3), ('Ecosport', 'SUV', 'azul', 2), 
-('Fox', 'Família', 'preto', 1);
+INSERT INTO model (name, category, color, brand_code) VALUES ('Gol', 'Hatch', 
+'black', 1), ('Palio', 'Hatch', 'red', 3), ('Ecosport', 'SUV', 'blue', 2), 
+('Fox', 'Family', 'black', 1);
 
-UPDATE modelo SET cor='cinza' WHERE codigo = 4;
+UPDATE model SET color='gray' WHERE code = 4;
 
-SELECT ma.nome, ma.pais, COUNT(mo.codigo_marca) AS qtd_carros
-FROM marca AS ma
-LEFT JOIN modelo AS mo
-ON ma.codigo = mo.codigo_marca
-GROUP BY ma.nome
+SELECT br.name, br.country, COUNT(mo.brand_code) AS qt_cars
+FROM brand AS br
+LEFT JOIN model AS mo
+ON br.code = mo.brand_code
+GROUP BY br.name
 
-DELETE FROM marca WHERE codigo = 4;
+DELETE FROM brand WHERE codigo = 4;
